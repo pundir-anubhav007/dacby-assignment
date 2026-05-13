be1166e4d4d5318e21e6a035e9c8d2ff1353b277
Anubhav Pundir <anubhavbhanu007@gmail.com>
2026-05-13 19:47:28 +0530

    added start script

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 const startServer = async () => {
   try {
